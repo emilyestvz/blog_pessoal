a60cabbf6c1825df9b655e4cf6a749d520e46c67
Emily <emilys@genstudents.org>
2025-01-15 10:13:27 -0300

    Finzalição CRUD Postagem :sparkles:

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -17,4 +17,36 @@ export class PostagemController {
     findall(): Promise<Postagem[]>{
         return this.postagemService.findAll();
     }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem>{
+        return this.postagemService.findById(id);
+    }
+
+    @Get('/titulo/:titulo') // 1º 'titulo': caminho | 2º 'titulo': variável
+    @HttpCode(HttpStatus.OK) //parseintpipe n precisa pois titulo é uma string
+    findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]>{
+        return this.postagemService.findByTitulo(titulo);
+    }
+    
+    // verbo POST para guardar a inserção de dados
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() postagem: Postagem): Promise<Postagem>{
+        return this.postagemService.create(postagem);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.update(postagem);
+    }
+
+    // verbo DELETE para excluir um registro
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.postagemService.delete(id);
+    }
 }
