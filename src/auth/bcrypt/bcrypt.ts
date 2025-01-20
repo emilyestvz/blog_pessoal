10afc442f04354a63f6efe02bf18faecf1e90cb8
Emily <emilys@genstudents.org>
2025-01-20 10:18:48 -0300

    :sparkles: finish

---
+++ src/auth/bcrypt/bcrypt.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class Bcrypt{
+
+    // Saltos significa qts vezes o algoritmo vai ser aplicado sobre a string que quero criptografar
+    async criptografarSenha(senha: string): Promise<string> {
+
+        let saltos: number = 10;
+        return await bcrypt.hash(senha, saltos)
+
+    }
+
+    async compararSenhas(senhaDigitada: string, senhaBanco: string): Promise<boolean> {
+        return await bcrypt.compare(senhaDigitada, senhaBanco);
+    }
+
+}
---
+++ src/auth/entities/usuarioLogin.entity.ts
@@ -0,0 +1,8 @@
+// Entidade Auxiliar, para armazenar o usuario e a senha (mas n gera tabela no banco de dados
+
+export class UsuarioLogin {
+
+    public usuario: string;
+
+    public senha: string;
+}
