c6856057ae9e0babccb3b49c56aea4effb81ae9f
Emily <emilys@genstudents.org>
2025-01-27 10:23:54 -0300

    :tada: Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
-import { Tema } from './tema/entities/tema.entity';
 import { AuthModule } from './auth/auth.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-      // logging: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+	    useClass: ProdService,
+      imports: [ConfigModule],
     }),
     PostagemModule,
     TemaModule,
@@ -32,5 +25,10 @@ import { AppController } from './app.controller';
 })
 export class AppModule {}
 
-/*[!CAUTION]
-Se a Classe AppController, não for registrada no Módulo principal da aplicação (AppModule), o endpoint do Swagger não ficará disponível. */
+/*
+- Se a Classe AppController, não for registrada no Módulo principal da aplicação (AppModule), o endpoint do Swagger não ficará disponível.
+
+- Para alternar entre as configurações Local e Remota, na Classe AppModule, na propriedade useClass, utilize uma das 2 opções abaixo:
+useClass: DevService 🡢 O Nest executará a aplicação com a configuração do Banco de dados local (MySQL)
+useClass: ProdService 🡢 O Nest executará a aplicação com a configuração do Banco de dados na nuvem (Render)
+*/
