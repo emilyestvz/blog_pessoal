eaa3392c93fd0cbaa47c0beda80f8a2863d9272a
Emily <emilys@genstudents.org>
2025-01-27 09:28:59 -0300

    :books: Inicio da documentação com Swagger e deploy com Render

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder() // construção do swagger
+  .setTitle('Blog Pessoal') // nome do projeto
+  .setDescription('Projeto Blog Pessoal') // descrição do projeto
+  .setContact('Emily Cristiny Dias', 'https://github.com/emilyestvz','emilyestvz@gmail.com') // informaçoes da pessoa dev
+  .setVersion('1.0') // versão
+  .addBearerAuth() // adc a autenticação com token jwt
+  .build(); // constrói a pag do swagger com tds os parâmetros acima
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   // mandando uma variavel de ambiente (Tz) dizendo q estamos em um horário -03:00
   process.env.TZ = '-03:00';
