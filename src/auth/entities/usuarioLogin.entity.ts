cc6da65bd605a5d5972f9bb64f50ce0c362fb9b5
Emily <emilys@genstudents.org>
2025-01-27 11:00:22 -0300

    :tada: Deploy

--- src/auth/entities/usuarioLogin.entity.ts
+++ src/auth/entities/usuarioLogin.entity.ts
@@ -1,5 +1,4 @@
 // Entidade Auxiliar, para armazenar o usuario e a senha (mas n gera tabela no banco de dados
-
 import { ApiProperty } from "@nestjs/swagger";
 
 export class UsuarioLogin {
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { JwtService } from '@nestjs/jwt';
 import { UsuarioService } from './../../usuario/services/usuario.service';
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Bcrypt } from '../bcrypt/bcrypt';
-import { UsuarioLogin } from '../entities/usuariologin.entity';
+import { UsuarioLogin } from '../entities/usuarioLogin.entity';
 
 
 @Injectable()
